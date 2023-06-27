a8a2d35fb3aacee334b7cfb4171425a80d368dbf
Manojkumar886 <maddymano434@gmail.com>
2023-06-27 18:26:30 +0530

    Components is over and usage of Export Keyword

---
+++ src/BasicComponents.js
@@ -0,0 +1,43 @@
+function Sampleone()
+{
+    return(
+        <>
+        <ol>Chocolate Name
+            <li>Nestle</li>
+            <li>Kitkat</li>
+            <li>MilkyBar</li>
+            <li>Park</li>
+        </ol>
+        </>
+    )
+}
+
+ let Sampletwo=()=>
+{
+    return(
+        <>
+        <a href="https://www.oracletutorial.com">Oracle</a>
+        </>
+    )
+}
+
+
+export function SampleThree()
+{
+    return(
+        <p>Thank You</p>
+    )
+}
+
+
+var SampleFour=()=>
+{
+    return(
+        <>
+        <b>This is Bold Tag</b></>
+    )
+}
+
+export {Sampleone,Sampletwo};
+
+export default SampleFour;
